f5cd5c8dca2c460f05996bcc7fc0b384c9e08063
Robescu Theodor <rbscthdr@protonmail.com>
2025-05-05 23:07:22 +0000

    removed try

--- task08/outputs.tf
+++ task08/outputs.tf
@@ -4,7 +4,6 @@ output "aci_fqdn" {
 }
 
 output "aks_lb_ip" {
-  description = "Load Balancer IP address of APP in AKS"
-  # Use try() to safely access the IP, return "Pending..." if not available yet
-  value = try(data.kubernetes_service.app_service.status[0].load_balancer[0].ingress[0].ip, "Pending...")
+  description = "Load balancer ip address req for the test"
+  value       = data.kubernetes_service.app.status.0.load_balancer.0.ingress.0.ip
 }
